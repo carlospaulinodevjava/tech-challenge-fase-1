d3c473155df0bed00f7ed9dfa86b096c33eb76e8
carlospaulinodevjava <carlospaulinodevjava@gmail.com>
2025-05-02 13:12:37 -0300

    TC-FASE1 criando metodo GET para listar usuarios paginados.
    TC-FASE1 criando metodo GET para listar usuarios paginados.

--- gestao/src/main/java/br/com/fiap/restaurante/gestao/presentation/controller/UsuarioController.java
+++ gestao/src/main/java/br/com/fiap/restaurante/gestao/presentation/controller/UsuarioController.java
@@ -1,29 +1,38 @@
 package br.com.fiap.restaurante.gestao.presentation.controller;
 
 import br.com.fiap.restaurante.gestao.application.usecase.impl.CriarUsuarioUseCase;
+import br.com.fiap.restaurante.gestao.application.usecase.impl.ListarUsuariosUseCase;
 import br.com.fiap.restaurante.gestao.presentation.dto.UsuarioDTO;
 import br.com.fiap.restaurante.gestao.presentation.dto.UsuarioResponseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
     private final CriarUsuarioUseCase criarUsuarioUseCase;
+    private final ListarUsuariosUseCase listarUsuariosUseCase;
 
-    public UsuarioController(CriarUsuarioUseCase criarUsuarioUseCase) {
+    public UsuarioController(CriarUsuarioUseCase criarUsuarioUseCase, ListarUsuariosUseCase listarUsuariosUseCase) {
         this.criarUsuarioUseCase = criarUsuarioUseCase;
+        this.listarUsuariosUseCase = listarUsuariosUseCase;
     }
 
     @PostMapping
     public ResponseEntity<UsuarioResponseDTO> criar(@RequestBody UsuarioDTO usuarioDTO) {
         var response = criarUsuarioUseCase.executar(usuarioDTO);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UsuarioResponseDTO>> listar(Pageable pageable){
+        var pagina = listarUsuariosUseCase.executar(pageable);
+        return ResponseEntity.ok(pagina);
     }
 
 }
