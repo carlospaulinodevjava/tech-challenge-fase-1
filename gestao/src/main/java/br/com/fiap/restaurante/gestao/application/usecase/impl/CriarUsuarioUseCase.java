5a4826fbf3494978c0c453544dc841ec17870441
carlospaulinodevjava <carlospaulinodevjava@gmail.com>
2025-05-01 21:34:40 -0300

    TC-FASE1 incluindo controller com POST para criar usuario
    TC-FASE1 incluindo controller com POST para criar usuario

---
+++ gestao/src/main/java/br/com/fiap/restaurante/gestao/application/usecase/impl/CriarUsuarioUseCase.java
@@ -0,0 +1,28 @@
+package br.com.fiap.restaurante.gestao.application.usecase.impl;
+
+import br.com.fiap.restaurante.gestao.domain.model.Usuario;
+import br.com.fiap.restaurante.gestao.domain.repository.UsuarioRepository;
+import br.com.fiap.restaurante.gestao.mapper.UsuarioMapper;
+import br.com.fiap.restaurante.gestao.presentation.dto.UsuarioDTO;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class CriarUsuarioUseCase {
+
+    private final UsuarioRepository usuarioRepository;
+    private final UsuarioMapper usuarioMapper;
+
+    public CriarUsuarioUseCase(UsuarioRepository usuarioRepository, UsuarioMapper usuarioMapper) {
+        this.usuarioRepository = usuarioRepository;
+        this.usuarioMapper = usuarioMapper;
+    }
+
+    public Usuario executar(UsuarioDTO dto) {
+        Usuario usuario = usuarioMapper.toEntity(dto);
+        usuario.setDataUltimaAlteracao(LocalDateTime.now());
+        usuario.getEnderecos().forEach(e -> e.setUsuario(usuario));
+        return usuarioRepository.save(usuario);
+    }
+}
---
+++ gestao/src/main/java/br/com/fiap/restaurante/gestao/presentation/controller/UsuarioController.java
@@ -0,0 +1,28 @@
+package br.com.fiap.restaurante.gestao.presentation.controller;
+
+import br.com.fiap.restaurante.gestao.application.usecase.impl.CriarUsuarioUseCase;
+import br.com.fiap.restaurante.gestao.presentation.dto.UsuarioDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    private final CriarUsuarioUseCase criarUsuarioUseCase;
+
+    public UsuarioController(CriarUsuarioUseCase criarUsuarioUseCase) {
+        this.criarUsuarioUseCase = criarUsuarioUseCase;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> criar(@RequestBody UsuarioDTO usuarioDTO) {
+        var usuario = criarUsuarioUseCase.executar(usuarioDTO);
+        return new ResponseEntity<>(usuario, HttpStatus.CREATED);
+    }
+
+}
